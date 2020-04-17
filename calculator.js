840ed424933ae1775074f108c4d7d676354a4709
MengZe <59787257+sudo-MEnGzE@users.noreply.github.com>
2020-04-17 12:48:44 +0800

    Added new features 'Algebra' and 'Calculus'

--- calculator.js
+++ calculator.js
@@ -1,8 +1,16 @@
 "use strist"
-
 // Get element <input> and <textarea> (output)
 let input = document.getElementById('input');
 let output = document.getElementById('output');
+
+let modeSelector = document.getElementById('selector');
+let mode = 'standard';
+
+modeSelector.addEventListener('change', userMode);
+
+function userMode() {
+    mode = modeSelector.value;
+}
 
 // When the user click 'Enter' key, assign the value in <input> to a new variable expression
 input.addEventListener("keyup", function(event){
@@ -12,15 +20,25 @@ input.addEventListener("keyup", function(event){
     }
 })
 
+let result = '';
+
 function calculate() {
     input.value = ''; // Everytime the user click 'Enter', the value in <input> reset
     let expression = window.expression;
-    let result  = math.evaluate(expression);
-    if(result == undefined) {
-        result = "no answer";
+
+    if(mode == 'standard') {
+        result = math.evaluate(expression);
+    } else if(mode == 'algebra'){
+        result = math.simplify(expression).toString();
+    } else {
+        result = math.derivative(expression, 'x').toString();
     }
+
+    if(result == undefined) result = "no answer";
+    
     output.value += ('Input:  ' + expression + '\n' + result + '\n' + '\n'); 
 }
+
 
 function reset() {
     input.value = '';
